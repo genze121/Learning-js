179a48c07bcd8fb7725a438859ee909fb8e8b7de
Tirtha Sharma <tirthagenze121@gmail.com>
2024-03-06 21:25:50 +0530

    Datatypes in JS, learned about types in JS, typeof()  for checking types.

---
+++ Datatypes/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3); // we are using node.js, not browser
+
+console.log(3 + 3);
+console.log("JavaScript");
+
+let age = 18; // number
+let userName = "JavaScript"; // string
+let isLoggedIn = false; // boolean
+
+/*Datatypes
+
+number -> 2 to the power 53
+string -> ""
+boolean -> true/false
+null -> standalone value
+undefined
+symbol -> unique
+object
+*/
+
+// To checks type in JS, we use typeof()
+
+console.log(typeof age); // number
+console.log(typeof userName); // string
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
