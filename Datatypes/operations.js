1b7ce32b569248f677fa09ea46a785ab7366b979
Tirtha Sharma <tirthagenze121@gmail.com>
2024-03-07 21:19:14 +0530

    Operations on operators.

---
+++ Datatypes/operations.js
@@ -0,0 +1,31 @@
+// *********************************** Operations ********************************************
+
+let value = 3;
+let negativeValue = -value;
+console.log(negativeValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+// console.log(2 ** 3);
+
+let str1 = "hello";
+let str2 = " JavaScript";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("12" + "12");
+console.log(12 + "12");
+console.log("12" + 12);
+console.log(1 + 2 + "12");
+console.log(2 - "1");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let counter = 1000;
+counter++; // postfix increment
+console.log(counter);
